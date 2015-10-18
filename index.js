b63443ade842c63a52bf95eacd01e9846edf54f2
Allain Lalonde <allain.lalonde@gmail.com>
2015-10-18 15:30:12 -0400

    adding callback support

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
 var isPromise = require('is-promise');
 var Promise = require('native-promise-only');
 
-module.exports = function (target, timeout, aborter) {
+module.exports = function impatient(target, timeout, aborter) {
   if (isPromise(target)) {
-    var aborted;
-
-    aborter = (aborter || target.abort || function() {}).bind(target);
-
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        if (aborted === undefined) {
-          aborted = true;
-          Promise.resolve(aborter()).then(function () {
-            reject(new Error('timeout'));
-          });
-        }
-      }, timeout);
-
-      target.then(function(result) {
-        if (aborted === undefined) {
-          aborted = false;
-          resolve(result);
-        }
-      }, function(err) {
-        if (aborted === undefined) {
-          aborted = false;
-          reject(err);
-        }
-      });
-    });
+    return impatientPromise(target, timeout, aborter);
+  } else if (typeof target === 'function') {
+    return impatientCallback(target, timeout, aborter);
   }
 };
+
+function impatientPromise(target, timeout, aborter) {
+  var aborted;
+
+  aborter = (aborter || target.abort || function() {}).bind(target);
+
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (aborted === undefined) {
+        aborted = true;
+        Promise.resolve(aborter()).then(function () {
+          reject(new Error('timeout'));
+        });
+      }
+    }, timeout);
+
+    target.then(processor(resolve), processor(reject));
+  });
+
+  function processor(process) {
+    return function(result) {
+      if (aborted === undefined) {
+        aborted = false;
+        process(result);
+      }
+    };
+  }
+}
+
+
+
+function impatientCallback(target, timeout, aborter) {
+  var _aborter;
+  if (aborter) {
+    _aborter = function() {
+      return new Promise(function(resolve) {
+        aborter(function (err) {
+          if (err) {
+            console.error(err);
+          }
+          resolve();
+        });
+      });
+    };
+  }
+
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    var cb = args.pop();
+
+    impatientPromise(new Promise(function(resolve, reject) {
+      args.push(function(err, result) {
+        return err ? reject(err) : resolve(result);
+      });
+      target.apply(null, args);
+    }), timeout, _aborter).then(function(result) {
+      cb(null, result);
+    }, function(err) {
+      cb(err);
+    });
+  };
+}
